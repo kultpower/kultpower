97bc5e5fca654fc7e9ffc014b152116acb6d91c7
sebeichholz <gene@oregami.org>
2016-01-17 22:08:39 +0100

    Spring boot init

---
+++ src/main/java/org/kultpower/KultpowerApplication.java
@@ -0,0 +1,12 @@
+package org.kultpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KultpowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KultpowerApplication.class, args);
+	}
+}
