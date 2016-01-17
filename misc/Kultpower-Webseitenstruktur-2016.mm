94792cd3d63337fa638686b4587e29cd58008171
sebeichholz <gene@oregami.org>
2016-01-17 21:52:25 +0100

    Mind Map

---
+++ misc/Kultpower-Webseitenstruktur-2016.mm
@@ -0,0 +1,253 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1451636883573" ID="ID_586105681" MODIFIED="1451650382773" STYLE="fork" TEXT="Kultpower">
+<font NAME="SansSerif" SIZE="20"/>
+<node COLOR="#0033ff" CREATED="1451636895314" ID="ID_1466390804" MODIFIED="1451648725089" POSITION="right" STYLE="fork" TEXT="Archiv mit &#xa;Komplettheften">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1451638877767" ID="ID_271675072" MODIFIED="1451648138784" STYLE="fork" TEXT="Scans">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451638880148" ID="ID_575468293" MODIFIED="1451648138786" STYLE="fork" TEXT="Inhalte">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451638883168" ID="ID_961767344" MODIFIED="1451648138788" STYLE="fork" TEXT="Suchfunktion">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451638886684" ID="ID_634923866" MODIFIED="1451648138791" STYLE="fork" TEXT="Spieleveteranen-Verkn&#xfc;pfung">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451638914605" ID="ID_138236466" MODIFIED="1451648138793" STYLE="fork" TEXT="Newsmeldungen/Updates">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1451636924194" ID="ID_417948287" MODIFIED="1451648503659" POSITION="right" STYLE="fork" TEXT="Spieletest-Datenbank">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1451637035845" ID="ID_1995354928" MODIFIED="1451648767168" STYLE="fork" TEXT="Zeitschriften">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1451637050756" ID="ID_1475751655" MODIFIED="1451648138799" STYLE="fork" TEXT="Cover-Scans">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637054111" ID="ID_1885540947" MODIFIED="1451648138800" STYLE="fork" TEXT="Editorial-Scan">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637057992" ID="ID_1292209899" MODIFIED="1451648138802" STYLE="fork" TEXT="Getestete Spiele">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637063120" ID="ID_379429855" MODIFIED="1451648138803" STYLE="fork" TEXT="Artikel">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1451637042995" ID="ID_1967167943" MODIFIED="1451648138805" STYLE="fork" TEXT="Spieletest-Sicht">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1451637075896" ID="ID_563429736" MODIFIED="1451648138806" STYLE="fork" TEXT="Wertung(en)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637080575" ID="ID_1722871189" MODIFIED="1451648138808" STYLE="fork" TEXT="Scan">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637083623" ID="ID_1630882721" MODIFIED="1451648138809" STYLE="fork" TEXT="zugeh&#xf6;rige Ausgabe">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451638356450" ID="ID_868909503" MODIFIED="1451648138811" STYLE="fork" TEXT="User-Bewertungen">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1451638721253" ID="ID_526178921" MODIFIED="1451648439424" STYLE="fork" TEXT="Chart-Generator f&#xfc;r Video Games &#xa;und Power Play Tests">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1451636904515" ID="ID_1728928044" MODIFIED="1451648514188" POSITION="right" STYLE="fork" TEXT="Mehr Mags:&#xa;Cover anderer Zeitschriften">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1451636942214" ID="ID_942801187" MODIFIED="1451648138816" POSITION="right" STYLE="fork" TEXT="Interviews">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1451638986978" ID="ID_576948997" MODIFIED="1451648138819" STYLE="fork" TEXT="Interview mit Michael Hengst vom 27.3.2001">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451638999360" ID="ID_545608214" MODIFIED="1451648138823" STYLE="fork" TEXT="Interview mit Winnie Forster vom 6.1.2003">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451639084645" ID="ID_740911125" MODIFIED="1451648138826" STYLE="fork" TEXT="Interview mit Winnie Forster, Juni 2005">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451639220236" ID="ID_1733761531" MODIFIED="1451648450952" STYLE="fork" TEXT="Interview mit Harald Horchler - Der Herausgeber der &#xa;Starkiller-B&#xfc;cher im Gespr&#xe4;ch mit Kultpower.de (Mai 2006) ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451639297390" ID="ID_1918591071" MODIFIED="1451648457738" STYLE="fork" TEXT="Interview mit Rolf Boyke - Der Zeichner der Starkiller-Comics &#xa;im Gespr&#xe4;ch mit Kultpower.de (Mai 2006)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451639118840" ID="ID_15432644" MODIFIED="1451648138863" STYLE="fork" TEXT="Interview mit Winnie Forster, Dezember 2008 ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451639166338" ID="ID_1960099873" MODIFIED="1451648466421" STYLE="fork" TEXT="Interview mit Winnie Forster, M&#xe4;rz 2011 - &#xa;Die Story hinter &quot;Volkscomputer&quot;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1451637146413" ID="ID_1065953694" MODIFIED="1451648423756" POSITION="left" STYLE="fork" TEXT="Speizal-Infos &#xa;zu Kult-Mags">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1451637158693" ID="ID_1728165637" MODIFIED="1451648138878" STYLE="fork" TEXT="Power Play">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1451637163655" ID="ID_861391136" MODIFIED="1451648138879" STYLE="fork" TEXT="Starkiller">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637168539" ID="ID_1316598453" MODIFIED="1451648138880" STYLE="fork" TEXT="Powertips">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451643901219" ID="ID_845331359" MODIFIED="1451648138881" STYLE="fork" TEXT="Ausgabe 2/91 komplett">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1451637192567" ID="ID_384098365" MODIFIED="1451648138883" STYLE="fork" TEXT="ASM">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1451637194664" ID="ID_1072608909" MODIFIED="1451648138884" STYLE="fork" TEXT="Feedback">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637197124" ID="ID_1195931821" MODIFIED="1451648138885" STYLE="fork" TEXT="Kopfnuss">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451644034875" ID="ID_1593747553" MODIFIED="1451648138886" STYLE="fork" TEXT="Erstausgabe komplett">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451644067776" ID="ID_821289974" MODIFIED="1451648138887" STYLE="fork" TEXT="&quot;Gesammelte Werke&quot; 1986-1994">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1451643912110" ID="ID_1427013308" MODIFIED="1451648138907" STYLE="fork" TEXT="Amiga Joker">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1451643915619" ID="ID_312509988" MODIFIED="1451648138909" STYLE="fork" TEXT="Erstausgabe komplett">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1451637201069" ID="ID_910798979" MODIFIED="1451648138890" STYLE="fork" TEXT="Video Games">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1451644131529" ID="ID_1082974366" MODIFIED="1452202272770" STYLE="fork" TEXT="Special:&#xa;11 Jahre &#xa;Videogames">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1451644187439" ID="ID_1158245129" MODIFIED="1451648138892" STYLE="fork" TEXT="Geburtstags-G&#xe4;stebuch ">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1451644191964" ID="ID_803875618" MODIFIED="1451648138893" STYLE="fork" TEXT="Alle Redakteure der Videogames">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1451644198042" ID="ID_1120653824" MODIFIED="1451648138894" STYLE="fork" TEXT="Geburtstags-Umfrage ">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1451644203478" ID="ID_811023272" MODIFIED="1451648138895" STYLE="fork" TEXT="Ansprache von Heinrich Lenhardt">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1451644208271" ID="ID_1098588009" MODIFIED="1451648138897" STYLE="fork" TEXT="VG-Erstausgabe komplett online">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1451644212850" ID="ID_88661367" MODIFIED="1451648138899" STYLE="fork" TEXT="VG-Special 2001 komplett online">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1451644138654" ID="ID_885185363" MODIFIED="1452202283624" STYLE="fork" TEXT="Special: &#xa;12 Jahre&#xa;Videogames">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1451644145576" ID="ID_1562241756" MODIFIED="1451648138901" STYLE="fork" TEXT=" Videogames-Hintergrundbilder ">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1451644157389" ID="ID_696686016" MODIFIED="1451648398297" STYLE="fork" TEXT="Markt &amp; Technik&#xa;Visitenkarten-Construction-Kit ">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1451644170544" ID="ID_861123898" MODIFIED="1451648138903" STYLE="fork" TEXT="Videogames-Statistiken">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1451644179328" ID="ID_1024122413" MODIFIED="1451648138904" STYLE="fork" TEXT="Videogames-Goodies">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1451637217219" ID="ID_1978161374" MODIFIED="1451650373628" STYLE="fork" TEXT="Ninja-Turkey-Comics &#xa;von Karl Bihlmeier">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1451638758047" ID="ID_1917086287" MODIFIED="1451648138934" POSITION="left" STYLE="fork" TEXT="Sonstiges">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1451637446832" ID="ID_1771722008" MODIFIED="1451648138936" STYLE="fork" TEXT="G&#xe4;stebuch">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451637460267" ID="ID_1750045973" MODIFIED="1451648138939" STYLE="fork" TEXT="Links auf andere Webseiten">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451638766584" ID="ID_1810886517" MODIFIED="1451648138942" STYLE="fork" TEXT="Impressum">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451638769450" ID="ID_163950559" MODIFIED="1451648138944" STYLE="fork" TEXT="Sitemap">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451638929335" ID="ID_191611921" MODIFIED="1451648138946" STYLE="fork" TEXT="Newsmeldungen/Updates">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1451637476172" ID="ID_393632311" MODIFIED="1451648409534" STYLE="fork" TEXT="Downloads">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1451637479743" ID="ID_574333179" MODIFIED="1451648409535" STYLE="fork" TEXT="Hintergrundbilder">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637484993" ID="ID_1665802502" MODIFIED="1451648409536" STYLE="fork" TEXT="Winamp-Skins">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1451637614471" ID="ID_1130898804" MODIFIED="1451648409538" STYLE="fork" TEXT="Retro-Stuff">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1451637714786" ID="ID_1765168466" MODIFIED="1451648409538" STYLE="fork" TEXT="Competition Pro USB">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637720088" ID="ID_1639097007" MODIFIED="1451648409539" STYLE="fork" TEXT="Immortal 3 CDs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1451637569653" ID="ID_1329554073" MODIFIED="1451648409541" STYLE="fork" TEXT="Retro-B&#xfc;cher">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1451637585425" ID="ID_770046281" MODIFIED="1451648409542" STYLE="fork" TEXT="B&#xfc;cher-Reviews">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1451637593256" ID="ID_355936566" MODIFIED="1451648409542" STYLE="fork" TEXT="B&#xfc;cher-Cover mit Link zu Amazon">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1451637413277" ID="ID_1244108264" MODIFIED="1451648409544" STYLE="fork" TEXT="3D-Renderings von Spielepackungen">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
