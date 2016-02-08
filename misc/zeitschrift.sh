b4edcaf702018586e07a83b30f5a16fbdf14278c
sebeichholz <gene@oregami.org>
2016-02-08 08:28:33 +0100

    Dateien zur Vorbereitung der Coverscans

---
+++ misc/zeitschrift.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
+cd $1;
+mkdir 100
+for img in `ls *.jpg`; do   convert -verbose -resize 100x141! -quality 85% $img ./100/$img; done
+mkdir 150
+for img in `ls *.jpg`; do   convert -verbose -resize 150x211! -quality 85% $img ./150/$img; done
+mkdir 200
+for img in `ls *.jpg`; do   convert -verbose -resize 200x282! -quality 85% $img ./200/$img; done
+mkdir 250
+for img in `ls *.jpg`; do   convert -verbose -resize 250x352! -quality 85% $img ./250/$img; done
+mkdir 300
+for img in `ls *.jpg`; do   convert -verbose -resize 300x423! -quality 85% $img ./300/$img; done
+cd ..
