e7ca13f4df15bc6d3412ef0366df3a38f59ff236
sebeichholz <gene@oregami.org>
2016-02-26 17:21:02 +0100

    disqus test

--- src/main/java/org/kultpower/HomeController.java
+++ src/main/java/org/kultpower/HomeController.java
@@ -1,6 +1,5 @@
 package org.kultpower;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 
-
-
 	@RequestMapping("/")
 	public String home(Model model) {
 		return "index";
+	}
+
+
+	@RequestMapping("/disqus")
+	public String disqus(Model model) {
+		return "disqus";
 	}
 }
